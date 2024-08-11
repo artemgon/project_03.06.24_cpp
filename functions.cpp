56bffc07df1dd9ad4b839d364f968f738f156e0d
artemgon <artemhoncharuk123@gmail.com>
2024-08-11 18:31:21 +0300

    Creating task 2

--- functions.cpp
+++ functions.cpp
@@ -1,1 +1,11 @@
 #include "functions.h"
+
+void starsFunction(int number)
+{
+	if (number > 0) {
+		cout << "* ";
+		number--;
+		starsFunction(number);
+	}
+	return;
+}
--- main.cpp
+++ main.cpp
@@ -2,5 +2,6 @@
 
 int main()
 {
+	starsFunction(5);
 	return 0;
 }
